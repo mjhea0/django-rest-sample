fbb1a642d684392db3f7857207a5bbe3bf177030
Michael Herman <hermanmu@gmail.com>
2013-12-20 22:34:40 -0600

    added model

--- appsuite/notes/models.py
+++ appsuite/notes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Note(models.Model):
+	owner = models.ForeignKey(User)
+	title = models.CharField(max_length=100)
+	text = models.CharField(max_length=500)
+	createTime = models.DateTimeField()
+
+
